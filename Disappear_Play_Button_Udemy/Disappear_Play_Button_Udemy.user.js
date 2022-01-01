13ce59fc5e6aed0922e916e945b44f40a9392899
Yash-Singh1 <saiansh2525@gmail.com>
2021-12-31 18:48:04 -0800

    feat(Disappear_Play_Button_Udemy): 0.1

---
+++ Disappear_Play_Button_Udemy/Disappear_Play_Button_Udemy.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         Disappear Play Button Udemy
+// @namespace    https://github.com/Yash-Singh1/UserScripts
+// @version      0.1
+// @description  Makes the play button disappear when the video is inactive to let you view the content (like other streaming platforms)
+// @author       Yash Singh
+// @match        https://*.udemy.com/course/*/learn/lecture/*
+// @icon         https://www.google.com/s2/favicons?domain=udemy.com
+// @grant        none
+// @homepage     https://github.com/Yash-Singh1/UserScripts/tree/main/Disappear_Play_Button_Udemy#readme
+// @homepageURL  https://github.com/Yash-Singh1/UserScripts/tree/main/Disappear_Play_Button_Udemy#readme
+// @supportURL   https://github.com/Yash-Singh1/UserScripts/issues
+// @license      MIT
+// @downloadURL  https://raw.githubusercontent.com/Yash-Singh1/UserScripts/main/Disappear_Play_Button_Udemy/Disappear_Play_Button_Udemy.user.js
+// @updateURL    https://raw.githubusercontent.com/Yash-Singh1/UserScripts/main/Disappear_Play_Button_Udemy/Disappear_Play_Button_Udemy.user.js
+// ==/UserScript==
+
+(async function () {
+  "use strict";
+
+  const observer = new MutationObserver(() => {
+    if (document.querySelector(".vjs-user-inactive")) {
+      document.querySelector(
+        '[class*="video-player--center--"]'
+      ).style.display = "none";
+    } else {
+      document.querySelector(
+        '[class*="video-player--center--"]'
+      ).style.display = "";
+    }
+  });
+  do {
+    if (document.querySelector('[class*="video-player--video-player"]')) {
+      observer.observe(
+        document.querySelector('[class*="video-player--video-player"]'),
+        {
+          attributes: true,
+        }
+      );
+      break;
+    } else {
+      await new Promise((resolve) => setTimeout(() => resolve(true), 200));
+    }
+  } while (true);
+})();
---
+++ Disappear_Play_Button_Udemy/README.md
@@ -0,0 +1,3 @@
+# Disappear Play Button Udemy
+
+Makes the play button disappear when the video is inactive to let you view the content (like other streaming platforms)
