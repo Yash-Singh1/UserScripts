9c76516bf4ab8272954ac02405737fcf611040be
Yash-Singh1 <saiansh2525@gmail.com>
2022-03-04 16:26:00 -0800

    feat(WordTune_Unlimited_Rewrites): 0.1

---
+++ WordTune_Unlimited_Rewrites/README.md
@@ -0,0 +1,3 @@
+# WordTune Unlimited Rewrites
+
+Allows you to do unlimited rewrites when using wordtune
---
+++ WordTune_Unlimited_Rewrites/WordTune_Unlimited_Rewrites.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         WordTune Unlimited Rewrites
+// @namespace    https://github.com/Yash-Singh1/UserScripts
+// @version      0.1
+// @description  Allows you to do unlimited rewrites when using wordtune
+// @author       Yash Singh
+// @match        https://www.wordtune.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=wordtune.com
+// @grant        none
+// @homepage     https://github.com/Yash-Singh1/UserScripts/tree/main/WordTune_Unlimited_Rewrites#readme
+// @homepageURL  https://github.com/Yash-Singh1/UserScripts/tree/main/WordTune_Unlimited_Rewrites#readme
+// @supportURL   https://github.com/Yash-Singh1/UserScripts/issues
+// @license      MIT
+// @downloadURL  https://raw.githubusercontent.com/Yash-Singh1/UserScripts/main/WordTune_Unlimited_Rewrites/WordTune_Unlimited_Rewrites.user.js
+// @updateURL    https://raw.githubusercontent.com/Yash-Singh1/UserScripts/main/WordTune_Unlimited_Rewrites/WordTune_Unlimited_Rewrites.user.js
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  setInterval(() => localStorage.setItem('lparc', '0'), 1000);
+})();
