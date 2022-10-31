f9dcb992a3db87b4fea9ebd90445960771c9adef
Yash-Singh1 <saiansh2525@gmail.com>
2022-10-30 20:51:00 -0700

    feat(USACO_Set_Default_Language): Initial Commit

---
+++ USACO_Set_Default_Language/README.md
@@ -0,0 +1,7 @@
+# USACO Set Default Language
+
+Set the default language in the dropdown for USACO.
+
+## Change Default Language
+
+To change the default language, select the "Set the default language" option in the Tampermonkey menu and fill out the given prompt with the exact spelling of the language as it is in the dropdown. The default language should be saved and automatically set.
---
+++ USACO_Set_Default_Language/USACO_Set_Default_Language.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         USACO Set Default Language
+// @namespace    https://github.com/Yash-Singh1/UserScripts
+// @version      0.1
+// @description  Set the default language in the dropdown for USACO
+// @author       Yash Singh
+// @match        http://www.usaco.org/index.php?page=viewproblem2&cpid=*
+// @icon         http://www.usaco.org/current/images/usaco_logo.png
+// @grant        none
+// @homepage     https://github.com/Yash-Singh1/UserScripts/tree/main/USACO_Set_Default_Language#readme
+// @homepageURL  https://github.com/Yash-Singh1/UserScripts/tree/main/USACO_Set_Default_Language#readme
+// @supportURL   https://github.com/Yash-Singh1/UserScripts/issues
+// @license      MIT
+// @downloadURL  https://raw.githubusercontent.com/Yash-Singh1/UserScripts/main/USACO_Set_Default_Language/USACO_Set_Default_Language.user.js
+// @updateURL    https://raw.githubusercontent.com/Yash-Singh1/UserScripts/main/USACO_Set_Default_Language/USACO_Set_Default_Language.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function changeDefault() {
+        let selectEl = document.querySelector('select[name="language"]');
+        let val = GM_getValue('lang', 'C++17');
+        let lookFor = '';
+        for (const optionEl of selectEl.children) {
+            if (optionEl.innerText === val) {
+                lookFor = optionEl.value;
+            }
+        }
+        if (lookFor === '') {
+            alert('Set language is not found')
+        } else {
+            selectEl.value = lookFor;
+        }
+    }
+
+    changeDefault();
+
+    GM_registerMenuCommand("Set the default language", () => {
+        GM_setValue('lang', prompt("What default language would you like to set to?", "C++17"));
+        changeDefault();
+    });
+})();
